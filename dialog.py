e38c5d22f6e10dfe948ea94410a954ecf3ca2f89
trungbmt <phamductrungbmt@gmail.com>
2023-06-14 08:43:30 +0700

    update requirement.txt

--- dialog.py
+++ dialog.py
@@ -11,8 +11,6 @@ from PyQt6.QtGui import *
 from PyQt6.QtCore import *
 from PyQt6.QtWebEngineWidgets import *
 from PyQt6.QtNetwork import QNetworkProxy
-import qtawesome as qta
-import asyncio
 class CustomDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
